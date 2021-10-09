69a9d56c86c23019597a213d4337b08eb1291faf
Eydel Rivero <eydel.rivero@supermean.com>
2021-10-09 08:15:03 +0000

    Implemented ddca create

--- ddca/programs/ddca/src/lib.rs
+++ ddca/programs/ddca/src/lib.rs
@@ -1,40 +1,135 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{ self, Mint, TokenAccount, Token, Transfer, CloseAccount };
+use anchor_spl::associated_token::AssociatedToken;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("3nmm1awnyhABJdoA25MYVksxz1xnpUFeepJJyRTZfsyD");
 
 #[program]
 pub mod ddca {
     use super::*;
-    
-    pub fn initialize(ctx: Context<Initialize>, data: u64) -> ProgramResult {
-        let my_account = &mut ctx.accounts.my_account;
-        my_account.data = data;
-        Ok(())
-    }
 
-    pub fn update(ctx: Context<Update>, data: u64) -> ProgramResult {
-        let my_account = &mut ctx.accounts.my_account;
-        my_account.data = data;
+    pub fn create_ddca(
+        ctx: Context<CreateInputAccounts>,
+        block_height: u64, 
+        pda_bump: u8,
+        from_initial_amount: u64,
+        from_amount_per_swap: u64,
+        interval_in_seconds: u64,
+        // last_completed_swap_ts: u64,
+    ) -> ProgramResult {
+
+        // for i in &block_height.to_be_bytes() {
+        //     msg!("{}", i);
+        // }
+
+        ctx.accounts.ddca_account.owner_acc_addr = *ctx.accounts.owner_account.key;
+        ctx.accounts.ddca_account.from_mint = *ctx.accounts.from_mint.as_ref().key; //ctx.accounts.from_token_account.mint;
+        ctx.accounts.ddca_account.from_tacc_addr =  *ctx.accounts.from_token_account.to_account_info().key; //*ctx.accounts.from_token_account.as_ref().key;
+        ctx.accounts.ddca_account.to_mint = *ctx.accounts.to_mint.as_ref().key;
+        ctx.accounts.ddca_account.to_tacc_addr =  *ctx.accounts.to_token_account.to_account_info().key;
+        ctx.accounts.ddca_account.block_height = block_height;
+        ctx.accounts.ddca_account.pda_bump = pda_bump;
+        ctx.accounts.ddca_account.from_initial_amount = from_initial_amount;
+        ctx.accounts.ddca_account.from_amount_per_swap = from_amount_per_swap;
+        ctx.accounts.ddca_account.interval_in_seconds = interval_in_seconds;
+        // ctx.accounts.ddca_account.last_completed_swap_ts = last_completed_swap_ts;
+
+        token::transfer(
+            ctx.accounts.into_transfer_to_vault_context(),
+            from_initial_amount,
+        )?;
+
         Ok(())
     }
 }
 
+// DERIVE ACCOUNTS
+
 #[derive(Accounts)]
-pub struct Initialize<'info> {
-    #[account(init, payer = user, space = 8 + 8)]
-    pub my_account: Account<'info, MyAccount>,
-    #[account(mut)]
-    pub user: Signer<'info>,
+#[instruction(
+    block_height: u64, 
+    pda_bump: u8,
+    from_initial_amount: u64,
+    from_amount_per_swap: u64,
+    // interval_in_seconds: u64,
+    // last_completed_swap_ts: u64,
+    )]
+pub struct CreateInputAccounts<'info> {
+    pub owner_account: Signer<'info>,
+    #[account(
+        mut,
+        constraint = owner_from_token_account.amount >= from_initial_amount
+    )]
+    pub owner_from_token_account: Box<Account<'info, TokenAccount>>,
+    #[account(
+        init, 
+        seeds = [
+            owner_account.key().as_ref(), 
+            &block_height.to_be_bytes(), 
+            b"ddca-seed"
+            ],
+        bump = pda_bump,
+        payer = owner_account, 
+        space = 8 + DdcaAccount::LEN)]
+    pub ddca_account: Account<'info, DdcaAccount>,
+    pub from_mint:  Account<'info, Mint>, 
+    #[account(
+        init, 
+        associated_token::mint = from_mint, 
+        associated_token::authority = ddca_account, 
+        payer = owner_account)]
+    pub from_token_account: Account<'info, TokenAccount>,
+    pub to_mint:  Account<'info, Mint>, 
+    #[account(
+        init, 
+        associated_token::mint = to_mint, 
+        associated_token::authority = ddca_account, 
+        payer = owner_account)]
+    pub to_token_account: Account<'info, TokenAccount>,
+    pub rent: Sysvar<'info, Rent>,
     pub system_program: Program<'info, System>,
+    pub token_program: AccountInfo<'info>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
 }
 
-#[derive(Accounts)]
-pub struct Update<'info> {
-    #[account(mut)]
-    pub my_account: Account<'info, MyAccount>,
-}
+// ACCOUNT STRUCTS
 
 #[account]
-pub struct MyAccount {
-    pub data: u64,
+pub struct DdcaAccount {
+    pub owner_acc_addr: Pubkey, //32 bytes
+    pub from_mint: Pubkey, //32 bytes
+    pub from_tacc_addr: Pubkey, //32 bytes
+    pub to_mint: Pubkey, //32 bytes
+    pub to_tacc_addr: Pubkey, //32 bytes
+    pub block_height: u64, //8 bytes
+    pub pda_bump: u8, //1 byte
+    pub from_initial_amount: u64, //8 bytes
+    pub from_amount_per_swap: u64, //8 bytes
+    pub start_ts: u64, //8 bytes
+    // pub frequency: u8, //TODO
+    pub interval_in_seconds: u64, //8 bytes
+    pub last_completed_swap_ts: u64, //8 bytes
+}
+
+impl DdcaAccount {
+    pub const LEN: usize = 32 + 32 + 32 + 32 + 32 + 8 + 1 + 8 + 8 + 8 + 8 + 8;
+}
+
+//UTILS IMPL
+
+impl<'info> CreateInputAccounts<'info> {
+    fn into_transfer_to_vault_context(
+        &self,
+    ) -> CpiContext<'_, '_, '_, 'info, Transfer<'info>> {
+        let cpi_accounts = Transfer {
+            from: self.owner_from_token_account.to_account_info().clone(),
+            to: self
+                .from_token_account
+                .to_account_info()
+                .clone(),
+            authority: self.owner_account.to_account_info().clone(),
+        };
+        let cpi_program = self.token_program.to_account_info();
+        CpiContext::new(cpi_program, cpi_accounts)
+    }
 }
--- ddca/tests/ddca.js
+++ ddca/tests/ddca.js
@@ -1,65 +1,218 @@
 const assert = require("assert");
 const anchor = require("@project-serum/anchor");
 const { SystemProgram } = anchor.web3;
+const { TOKEN_PROGRAM_ID, ASSOCIATED_TOKEN_PROGRAM_ID, Token } = require("@solana/spl-token");
 
 describe("ddca", () => {
-  // Use a local provider.
-  const provider = anchor.Provider.local();
-
   // Configure the client to use the local cluster.
+  const provider = anchor.Provider.env();
   anchor.setProvider(provider);
 
-  it("Creates and initializes an account in a single atomic transaction (simplified)", async () => {
-    // #region code-simplified
-    // The program to execute.
-    const program = anchor.workspace.Ddca;
+  //
+  const SYSTEM_PROGRAM_ID = anchor.web3.SystemProgram.programId;
+  const program = anchor.workspace.Ddca;
+  const payer = anchor.web3.Keypair.generate();
+  const mintAuthority = anchor.web3.Keypair.generate();
+  let mintA = null;
+  let mintB = null;
+  let splTokenAClient = null;
+  let splTokenBClient = null;
 
-    // The Account to create.
-    const myAccount = anchor.web3.Keypair.generate();
+  let ownerTokenAccountAAddress = null;
+  const ownerTokenAccountAInitialBalance = 500;
 
-    // Create the new account and initialize it with the program.
-    // #region code-simplified
-    await program.rpc.initialize(new anchor.BN(1234), {
-      accounts: {
-        myAccount: myAccount.publicKey,
-        user: provider.wallet.publicKey,
-        systemProgram: SystemProgram.programId,
-      },
-      signers: [myAccount],
-    });
-    // #endregion code-simplified
+  const ddcaFromInitialAmount = 100;
+  const ddcaFromAmountPerSwap = 10;
+  const ddcaIntervalInSeconds = 5 * 60; //5 minutes
 
-    // Fetch the newly created account from the cluster.
-    const account = await program.account.myAccount.fetch(myAccount.publicKey);
+  it("One-time test setup", async () => {
+    // Airdropping tokens to a payer.
+    await provider.connection.confirmTransaction(
+      await provider.connection.requestAirdrop(payer.publicKey, 10000000000),
+      "confirmed"
+    );
 
-    // Check it's state was initialized.
-    assert.ok(account.data.eq(new anchor.BN(1234)));
+    mintA = await Token.createMint(
+      provider.connection,
+      payer,
+      mintAuthority.publicKey,
+      null,
+      0,
+      TOKEN_PROGRAM_ID
+    );
 
-    // Store the account for the next test.
-    _myAccount = myAccount;
+    ownerTokenAccountAAddress = await mintA.createAccount(provider.wallet.publicKey);
+
+    await mintA.mintTo(
+      ownerTokenAccountAAddress,
+      mintAuthority.publicKey,
+      [mintAuthority],
+      ownerTokenAccountAInitialBalance
+    );
+
+    let _ownerTokenAccountA = await mintA.getAccountInfo(ownerTokenAccountAAddress);
+    assert.ok(_ownerTokenAccountA.amount.toNumber() == ownerTokenAccountAInitialBalance);
+
+    mintB = await Token.createMint(
+      provider.connection,
+      payer,
+      mintAuthority.publicKey,
+      null,
+      0,
+      TOKEN_PROGRAM_ID
+    );
+
+    splTokenAClient = new Token(
+      program.provider.connection,
+      mintA.publicKey,
+      TOKEN_PROGRAM_ID,
+      program.provider.wallet.payer
+    );
+
+    splTokenBClient = new Token(
+      program.provider.connection,
+      mintB.publicKey,
+      TOKEN_PROGRAM_ID,
+      program.provider.wallet.payer
+    );
+
   });
 
-  it("Updates a previously created account", async () => {
-    const myAccount = _myAccount;
+  it("Should create ddca", async () => {
+    //CREATE DDCA
 
-    // #region update-test
+    // const timestampSeed = Date.now().toString();
+    const blockHeight = await program.provider.connection.getSlot('confirmed');
+    const blockHeightBytes = new anchor.BN(blockHeight).toBuffer('be', 8);
 
-    // The program to execute.
-    const program = anchor.workspace.Ddca;
+    //ddca account pda and bump
+    const [ddcaAccountPda, ddcaAccountPdaBump] = await anchor.web3.PublicKey.findProgramAddress(
+      [
+        provider.wallet.publicKey.toBuffer(),
+        blockHeightBytes,
+        Buffer.from(anchor.utils.bytes.utf8.encode("ddca-seed")),
+      ],
+      program.programId
+    );
 
-    // Invoke the update rpc.
-    await program.rpc.update(new anchor.BN(4321), {
-      accounts: {
-        myAccount: myAccount.publicKey,
-      },
-    });
+    //ddca associated token account (from)
+    const ddcaFromTokenAccountAddress = await Token.getAssociatedTokenAddress(
+      associatedProgramId=ASSOCIATED_TOKEN_PROGRAM_ID,
+      programId=TOKEN_PROGRAM_ID,
+      mint=mintA.publicKey,
+      owner=ddcaAccountPda,
+      allowOwnerOffCurve=true,
+    );
+    //ddca associated token account (to)
+    const ddcaToTokenAccountAddress = await Token.getAssociatedTokenAddress(
+      associatedProgramId=ASSOCIATED_TOKEN_PROGRAM_ID,
+      programId=TOKEN_PROGRAM_ID,
+      mint=mintB.publicKey,
+      owner=ddcaAccountPda,
+      allowOwnerOffCurve=true,
+    );
 
-    // Fetch the newly updated account.
-    const account = await program.account.myAccount.fetch(myAccount.publicKey);
+    let ownerAccountLamports = (
+      await program.provider.connection.getAccountInfo(
+        program.provider.wallet.publicKey
+      )
+    ).lamports;
 
-    // Check it's state was mutated.
-    assert.ok(account.data.eq(new anchor.BN(4321)));
+    console.log("TEST PARAMETERS:")
+    console.log("  Program ID:                         " + program.programId);
+    console.log("  mintAuthority.publicKey:            " + mintAuthority.publicKey);
+    console.log("  mintA.publicKey:                    " + mintA.publicKey);
+    console.log("  mintB.publicKey:                    " + mintB.publicKey);
+    console.log("  ownerAccountAddress:                " + provider.wallet.publicKey);
+    console.log("  ownerAccount.lamports:              " + ownerAccountLamports);
+    console.log("  ownerFromTokenAccountAddress:       " + ownerTokenAccountAAddress);
+    console.log("  blockHeight:                        " + blockHeight);
+    console.log("  ddcaAccountPda:                     " + ddcaAccountPda);
+    console.log("  ddcaAccountPdaBump:                 " + ddcaAccountPdaBump);
+    console.log("  ddcaFromTokenAccountAddress:        " + ddcaFromTokenAccountAddress);
+    console.log("  SYSTEM_PROGRAM_ID:                  " + SYSTEM_PROGRAM_ID);
+    console.log("  TOKEN_PROGRAM_ID:                   " + TOKEN_PROGRAM_ID);
+    console.log();
+    
+    const createTx = await program.rpc.createDdca(new anchor.BN(blockHeight), ddcaAccountPdaBump,
+      new anchor.BN(ddcaFromInitialAmount), new anchor.BN(ddcaFromAmountPerSwap), new anchor.BN(ddcaIntervalInSeconds), 
+      {
+        accounts: {
+          ownerAccount: provider.wallet.publicKey,
+          ownerFromTokenAccount: ownerTokenAccountAAddress,
+          ddcaAccount: ddcaAccountPda,
+          fromMint: mintA.publicKey,
+          fromTokenAccount: ddcaFromTokenAccountAddress,
+          toMint: mintB.publicKey,
+          toTokenAccount: ddcaToTokenAccountAddress,
+          rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+          systemProgram: SYSTEM_PROGRAM_ID,
+          tokenProgram: TOKEN_PROGRAM_ID,
+          associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID
+        },
+      }
+    );
 
-    // #endregion update-test
+    await logPostTransactionState("Create", createTx);
+
+    // const accounts = await program.account.vaultAccount.all(provider.wallet.publicKey.toBuffer()); //uncomment to list vault accounts owned by current wallet: provider.wallet.publicKey
+    // const accounts = await program.account.vaultAccount.all(anchor.web3.Keypair.generate().publicKey.toBuffer()); //this will fetch empty list
+    // console.log(accounts);
+
+    const _ddcaAccount = await program.account.ddcaAccount.fetch(ddcaAccountPda);
+    assert.ok(_ddcaAccount.ownerAccAddr.equals(provider.wallet.publicKey));
+    assert.ok((_ddcaAccount.bump = ddcaAccountPdaBump));
+    assert.ok((_ddcaAccount.fromTaccAddress = ddcaFromTokenAccountAddress));
+
+    let _ddcaFromTokenAccount = await splTokenAClient.getAccountInfo(ddcaFromTokenAccountAddress);
+    assert.ok(_ddcaFromTokenAccount.state === 1);
+    assert.ok(_ddcaFromTokenAccount.amount.toNumber() === ddcaFromInitialAmount);
+    assert.ok(_ddcaFromTokenAccount.isInitialized);
+    assert.ok(_ddcaFromTokenAccount.owner.equals(ddcaAccountPda));
+    assert.ok(_ddcaFromTokenAccount.mint.equals(mintA.publicKey));
+    
+
+    //UTILS
+    async function logPostTransactionState(transactionName, transactionSignature){
+
+      console.log("AFTER %s:", transactionName.toUpperCase());
+
+      // const tx = await provider.connection.getTransaction(transactionSignature, 'confirmed');
+      // console.log(tx);
+      
+      console.log("  ownerAccountLamports.lamports:     " + (await getLamports(program.provider.wallet.publicKey)));
+      console.log("  ownerTokenAccount.amount:          " + (await getTokenAAmount(ownerTokenAccountAAddress)));
+      console.log();
+      console.log("  ddcaAccount.lamports:              " + (await getLamports(ddcaAccountPda)));
+      console.log("  ddcaAccount.cli:                   " + "solana account %s", ddcaAccountPda);
+      console.log();
+      console.log("  ddcaFromTokenAccount.lamports:     " + (await getLamports(ddcaFromTokenAccountAddress)));
+      console.log("  ddcaFromTokenAccount.amount:       " + (await getTokenAAmount(ddcaFromTokenAccountAddress)));
+      console.log("  ddcaFromTokenAccount.cli (system): " + "solana account %s", ddcaFromTokenAccountAddress);
+      console.log("  ddcaFromTokenAccount.cli (token):  " + "spl-token account-info --address %s", ddcaFromTokenAccountAddress);
+      console.log();
+      console.log("  ddcaToTokenAccount.lamports:       " + (await getLamports(ddcaToTokenAccountAddress)));
+      console.log("  ddcaToTokenAccount.amount:         " + (await getTokenBAmount(ddcaToTokenAccountAddress)));
+      console.log("  ddcaToTokenAccount.cli (system):   " + "solana account %s", ddcaToTokenAccountAddress);
+      console.log("  ddcaToTokenAccount.cli (token):    " + "spl-token account-info --address %s", ddcaToTokenAccountAddress);
+      console.log();
+      console.log("  transaction:                       " + transactionSignature);
+      console.log("  transaction.cli:                   " + "solana confirm %s -v", transactionSignature);
+      console.log();
+    }
+
+    async function getLamports(accountAddress){
+      return (await program.provider.connection.getAccountInfo(accountAddress)).lamports;
+    }
+
+    async function getTokenAAmount(tokenAccountAddress){
+      return(await splTokenAClient.getAccountInfo(tokenAccountAddress)).amount;
+    }
+
+    async function getTokenBAmount(tokenAccountAddress){
+      return(await splTokenBClient.getAccountInfo(tokenAccountAddress)).amount;
+    }
+
   });
+
 });
