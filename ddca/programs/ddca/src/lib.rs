be6e8bce7941747e55f499ae6eb9fad9209e7fcc
Eydel Rivero <eydel.rivero@supermean.com>
2021-10-12 09:56:35 +0000

    More word done on 'create' (fees, initial deposit, gas budget, account params)

--- ddca/programs/ddca/src/lib.rs
+++ ddca/programs/ddca/src/lib.rs
@@ -2,25 +2,52 @@ use anchor_lang::prelude::*;
 use anchor_spl::token::{ self, Mint, TokenAccount, Token, Transfer, CloseAccount };
 use anchor_spl::associated_token::AssociatedToken;
 
+// Constants.
+pub mod ddca_operating_account {
+    solana_program::declare_id!("3oSfkjQZKCneYvsCTZc9HViGAPqR8pYr4h9YeGB5ZxHf");
+}
+
+// hybrid_liquidity_aggregator_program
+pub mod hla_program {
+    solana_program::declare_id!("5sEgjVKG4pNUrjU1EVmKRsEAmsB9f2ujJn2H1ZxX2UQs");
+}
+pub mod hla_operating_account {
+    solana_program::declare_id!("3oSfkjQZKCneYvsCTZc9HViGAPqR8pYr4h9YeGB5ZxHf");
+}
+
+pub const CREATE_FLAT_LAMPORT_FEE: u64 = 10000;
+pub const ADD_FUNDS_PERCENT_TOKEN_FEE: f64 = 0.003;
+pub const CREATE_WITH_FUNDS_PERCENT_TOKEN_FEE: f64 = 0.003;
+pub const WITHDRAW_PERCENT_TOKEN_FEE: f64 = 0.0005;
+pub const STOP_FLAT_LAMPORT_FEE: u64 = 10000;
+pub const START_FLAT_LAMPORT_FEE: u64 = 10000;
+pub const LAMPORTS_PER_SOL: u64 = 1000000000;
+pub const SINGLE_SWAP_MINIMUM_LAMPORT_FEE: u64 = 20000000; //20 million
+
 declare_id!("3nmm1awnyhABJdoA25MYVksxz1xnpUFeepJJyRTZfsyD");
 
 #[program]
 pub mod ddca {
     use super::*;
 
-    pub fn create_ddca(
+    pub fn create(
         ctx: Context<CreateInputAccounts>,
         block_height: u64, 
         pda_bump: u8,
         from_initial_amount: u64,
         from_amount_per_swap: u64,
         interval_in_seconds: u64,
-        // last_completed_swap_ts: u64,
+        min_out_amount: u64,
+        slippage: u8,
     ) -> ProgramResult {
 
         // for i in &block_height.to_be_bytes() {
         //     msg!("{}", i);
         // }
+
+        if ctx.remaining_accounts.len() == 0 {
+            return Err(ProgramError::Custom(1)); // Arbitrary error.
+        }
 
         ctx.accounts.ddca_account.owner_acc_addr = *ctx.accounts.owner_account.key;
         ctx.accounts.ddca_account.from_mint = *ctx.accounts.from_mint.as_ref().key; //ctx.accounts.from_token_account.mint;
@@ -32,12 +59,73 @@ pub mod ddca {
         ctx.accounts.ddca_account.from_initial_amount = from_initial_amount;
         ctx.accounts.ddca_account.from_amount_per_swap = from_amount_per_swap;
         ctx.accounts.ddca_account.interval_in_seconds = interval_in_seconds;
-        // ctx.accounts.ddca_account.last_completed_swap_ts = last_completed_swap_ts;
 
+        if from_initial_amount == 0 {
+            // transfer LAMPORT flat fees to the ddca operating account
+            msg!("flat fees: transfering {} lamports from owner to operating account", CREATE_FLAT_LAMPORT_FEE);
+            let ix = anchor_lang::solana_program::system_instruction::transfer(
+                ctx.accounts.owner_account.key,
+                ctx.accounts.operating_account.key,
+                CREATE_FLAT_LAMPORT_FEE,
+            );
+
+            anchor_lang::solana_program::program::invoke(
+                &ix,
+                &[
+                    ctx.accounts.owner_account.to_account_info(),
+                    ctx.accounts.operating_account.to_account_info(),
+                ],
+            )?;
+
+            return Ok(());
+        }
+
+        if from_initial_amount % from_amount_per_swap != 0 {
+            return Err(ErrorCode::InvalidAmounts.into());
+        }
+        
+        let swap_count: u64 = (from_initial_amount / from_amount_per_swap) - 1; // -1: first swap will occur in the current transaction
+        if swap_count == 0 {
+            return Err(ErrorCode::InvalidSwapsCount.into());
+        }
+
+        // transfer Token percentage fee to the ddca operating account
+        let from_mint_decimals = ctx.accounts.from_mint.decimals;
+        // msg!("from_mint_decimals: {}", from_mint_decimals);
+        let add_funds_fee = spl_token::ui_amount_to_amount(from_initial_amount as f64 * CREATE_WITH_FUNDS_PERCENT_TOKEN_FEE, from_mint_decimals);
+        msg!("'create with funds' fee: {}", add_funds_fee);
+        if add_funds_fee > 0 {
+            token::transfer(
+                ctx.accounts.into_transfer_fee_to_operating_context(),
+                add_funds_fee,
+            )?;
+        }
+
+        // transfer enough SOL gas budget to the ddca account to pay future recurring swaps fees
+        let recurring_lamport_fees = swap_count * SINGLE_SWAP_MINIMUM_LAMPORT_FEE;
+        msg!("transfering {} lamports ({} SOL) from owner to ddca account for next {} swaps", recurring_lamport_fees, recurring_lamport_fees as f64 / LAMPORTS_PER_SOL as f64, swap_count);
+        let ix = anchor_lang::solana_program::system_instruction::transfer(
+            ctx.accounts.owner_account.key,
+            ctx.accounts.ddca_account.as_ref().key,
+            recurring_lamport_fees,
+        );
+
+        anchor_lang::solana_program::program::invoke(
+            &ix,
+            &[
+                ctx.accounts.owner_account.to_account_info(),
+                ctx.accounts.ddca_account.to_account_info(),
+            ],
+        )?;
+
+        // transfer Token initial amount to ddca 'from' token account
         token::transfer(
             ctx.accounts.into_transfer_to_vault_context(),
             from_initial_amount,
         )?;
+
+        // execute the first swap
+        // TODO:
 
         Ok(())
     }
@@ -51,16 +139,17 @@ pub mod ddca {
     pda_bump: u8,
     from_initial_amount: u64,
     from_amount_per_swap: u64,
-    // interval_in_seconds: u64,
-    // last_completed_swap_ts: u64,
     )]
 pub struct CreateInputAccounts<'info> {
+    // owner
+    #[account(mut)]
     pub owner_account: Signer<'info>,
     #[account(
         mut,
         constraint = owner_from_token_account.amount >= from_initial_amount
     )]
     pub owner_from_token_account: Box<Account<'info, TokenAccount>>,
+    // ddca
     #[account(
         init, 
         seeds = [
@@ -70,7 +159,9 @@ pub struct CreateInputAccounts<'info> {
             ],
         bump = pda_bump,
         payer = owner_account, 
-        space = 8 + DdcaAccount::LEN)]
+        space = 8 + DdcaAccount::LEN,
+        constraint = from_amount_per_swap > 0,
+    )]
     pub ddca_account: Account<'info, DdcaAccount>,
     pub from_mint:  Account<'info, Mint>, 
     #[account(
@@ -78,14 +169,33 @@ pub struct CreateInputAccounts<'info> {
         associated_token::mint = from_mint, 
         associated_token::authority = ddca_account, 
         payer = owner_account)]
-    pub from_token_account: Account<'info, TokenAccount>,
+    pub from_token_account: Box<Account<'info, TokenAccount>>,
+    #[account(constraint = from_mint.key() != to_mint.key())]
     pub to_mint:  Account<'info, Mint>, 
     #[account(
         init, 
         associated_token::mint = to_mint, 
         associated_token::authority = ddca_account, 
         payer = owner_account)]
-    pub to_token_account: Account<'info, TokenAccount>,
+    pub to_token_account: Box<Account<'info, TokenAccount>>,
+    #[account(mut, address = ddca_operating_account::ID)]
+    pub operating_account: AccountInfo<'info>,
+    #[account(
+        mut,
+        //TODO: uncomment when https://github.com/project-serum/anchor/pull/843 is released
+        // associated_token::mint = from_mint, 
+        // associated_token::authority = ddca_operating_account, 
+    )]
+    pub operating_from_token_account: Box<Account<'info, TokenAccount>>,
+    // Hybrid Liquidity Aggregator
+    #[account(address = hla_program::ID)]
+    pub hla_program: AccountInfo<'info>,
+    pub hla_operating_account: AccountInfo<'info>,
+    pub hla_operating_from_token_account: Box<Account<'info, TokenAccount>>,
+    // pub hla_protocol_account: AccountInfo<'info>, // remaining account
+    // pub hla_pool_account: AccountInfo<'info>, // remaining account
+    // pub hla_amm_account: AccountInfo<'info>, // remaining account
+    // system and spl
     pub rent: Sysvar<'info, Rent>,
     pub system_program: Program<'info, System>,
     pub token_program: AccountInfo<'info>,
@@ -106,7 +216,6 @@ pub struct DdcaAccount {
     pub from_initial_amount: u64, //8 bytes
     pub from_amount_per_swap: u64, //8 bytes
     pub start_ts: u64, //8 bytes
-    // pub frequency: u8, //TODO
     pub interval_in_seconds: u64, //8 bytes
     pub last_completed_swap_ts: u64, //8 bytes
 }
@@ -132,4 +241,27 @@ impl<'info> CreateInputAccounts<'info> {
         let cpi_program = self.token_program.to_account_info();
         CpiContext::new(cpi_program, cpi_accounts)
     }
+
+    fn into_transfer_fee_to_operating_context(
+        &self,
+    ) -> CpiContext<'_, '_, '_, 'info, Transfer<'info>> {
+        let cpi_accounts = Transfer {
+            from: self.owner_from_token_account.to_account_info().clone(),
+            to: self
+                .operating_from_token_account
+                .to_account_info()
+                .clone(),
+            authority: self.owner_account.to_account_info().clone(),
+        };
+        let cpi_program = self.token_program.to_account_info();
+        CpiContext::new(cpi_program, cpi_accounts)
+    }
+}
+
+#[error]
+pub enum ErrorCode {
+    #[msg("Deposit amount must be a multiple of the amount per swap")]
+    InvalidAmounts,
+    #[msg("The number of recurring swaps must be greater than 1")]
+    InvalidSwapsCount,
 }
--- ddca/tests/ddca.js
+++ ddca/tests/ddca.js
@@ -1,17 +1,27 @@
 const assert = require("assert");
 const anchor = require("@project-serum/anchor");
-const { SystemProgram } = anchor.web3;
+const { SystemProgram, PublicKey } = anchor.web3;
 const { TOKEN_PROGRAM_ID, ASSOCIATED_TOKEN_PROGRAM_ID, Token } = require("@solana/spl-token");
+const FAILED_TO_FIND_ACCOUNT = 'Failed to find account';
+const INVALID_ACCOUNT_OWNER = 'Invalid account owner';
 
 describe("ddca", () => {
   // Configure the client to use the local cluster.
   const provider = anchor.Provider.env();
+  // const provider = anchor.Provider.local(url="http://localhost:8899", opts= {
+  //   preflightCommitment: "confirmed",
+  //   commitment: "confirmed",
+  // });
   anchor.setProvider(provider);
 
   //
+  const DDCA_OPERATING_ACCOUNT_ADDRESS = new PublicKey("3oSfkjQZKCneYvsCTZc9HViGAPqR8pYr4h9YeGB5ZxHf");
   const SYSTEM_PROGRAM_ID = anchor.web3.SystemProgram.programId;
+  const LAMPORTS_PER_SOL = anchor.web3.LAMPORTS_PER_SOL;
   const program = anchor.workspace.Ddca;
   const payer = anchor.web3.Keypair.generate();
+  const ownerAccount = provider.wallet.payer; //anchor.web3.Keypair.generate(); 
+  const ownerAccountAddress = ownerAccount.publicKey;
   const mintAuthority = anchor.web3.Keypair.generate();
   let mintA = null;
   let mintB = null;
@@ -25,10 +35,23 @@ describe("ddca", () => {
   const ddcaFromAmountPerSwap = 10;
   const ddcaIntervalInSeconds = 5 * 60; //5 minutes
 
+  // Hybrid Liquidity Aggregator accounts
+  const HLA_PROGRAM_ADDRESS = new PublicKey("5sEgjVKG4pNUrjU1EVmKRsEAmsB9f2ujJn2H1ZxX2UQs");
+  const HLA_OPERATING_ACCOUNT_ADDRESS = anchor.web3.Keypair.generate().publicKey; //TODO: to be provided
+  const hlaProtocolAddress = anchor.web3.Keypair.generate().publicKey;
+  const hlaPoolAddress = anchor.web3.Keypair.generate().publicKey;
+  const hlaAmmAddress = anchor.web3.Keypair.generate().publicKey;
+
   it("One-time test setup", async () => {
     // Airdropping tokens to a payer.
     await provider.connection.confirmTransaction(
       await provider.connection.requestAirdrop(payer.publicKey, 10000000000),
+      "confirmed"
+    );
+    
+    // Airdropping tokens to a payer.
+    await provider.connection.confirmTransaction(
+      await provider.connection.requestAirdrop(ownerAccountAddress, 1000000000),
       "confirmed"
     );
 
@@ -37,19 +60,17 @@ describe("ddca", () => {
       payer,
       mintAuthority.publicKey,
       null,
-      0,
+      1,
       TOKEN_PROGRAM_ID
     );
 
-    ownerTokenAccountAAddress = await mintA.createAccount(provider.wallet.publicKey);
-
+    ownerTokenAccountAAddress = await mintA.createAssociatedTokenAccount(ownerAccountAddress);
     await mintA.mintTo(
       ownerTokenAccountAAddress,
       mintAuthority.publicKey,
       [mintAuthority],
       ownerTokenAccountAInitialBalance
     );
-
     let _ownerTokenAccountA = await mintA.getAccountInfo(ownerTokenAccountAAddress);
     assert.ok(_ownerTokenAccountA.amount.toNumber() == ownerTokenAccountAInitialBalance);
 
@@ -58,7 +79,7 @@ describe("ddca", () => {
       payer,
       mintAuthority.publicKey,
       null,
-      0,
+      1,
       TOKEN_PROGRAM_ID
     );
 
@@ -88,7 +109,7 @@ describe("ddca", () => {
     //ddca account pda and bump
     const [ddcaAccountPda, ddcaAccountPdaBump] = await anchor.web3.PublicKey.findProgramAddress(
       [
-        provider.wallet.publicKey.toBuffer(),
+        ownerAccountAddress.toBuffer(),
         blockHeightBytes,
         Buffer.from(anchor.utils.bytes.utf8.encode("ddca-seed")),
       ],
@@ -112,55 +133,127 @@ describe("ddca", () => {
       allowOwnerOffCurve=true,
     );
 
-    let ownerAccountLamports = (
-      await program.provider.connection.getAccountInfo(
-        program.provider.wallet.publicKey
-      )
-    ).lamports;
+    //ddca operating token account (from)
+    const ddcaOperatingFromTokenAccountAddress = await Token.getAssociatedTokenAddress(
+      associatedProgramId=ASSOCIATED_TOKEN_PROGRAM_ID,
+      programId=TOKEN_PROGRAM_ID,
+      mint=mintA.publicKey,
+      owner=DDCA_OPERATING_ACCOUNT_ADDRESS,
+    );
+
+    //hla operating token account (from)
+    const hlaOperatingFromTokenAccountAddress = await Token.getAssociatedTokenAddress(
+      associatedProgramId=ASSOCIATED_TOKEN_PROGRAM_ID,
+      programId=TOKEN_PROGRAM_ID,
+      mint=mintA.publicKey,
+      owner=HLA_OPERATING_ACCOUNT_ADDRESS,
+    );
+
+    // Instructions
+    let instructions = [];
+
+    let ddcaOperatingAtaCreateInstruction = await createAtaCreateInstructionIfNotExists(
+      ddcaOperatingFromTokenAccountAddress, 
+      mintA.publicKey, 
+      DDCA_OPERATING_ACCOUNT_ADDRESS, 
+      provider.wallet.payer.publicKey,
+      splTokenAClient);
+    if(ddcaOperatingFromTokenAccountAddress !== null)
+      instructions.push(ddcaOperatingAtaCreateInstruction);
+    
+    let hlaOperatingAtaCreateInstruction = await createAtaCreateInstructionIfNotExists(
+      hlaOperatingFromTokenAccountAddress, 
+      mintA.publicKey, 
+      HLA_OPERATING_ACCOUNT_ADDRESS, 
+      provider.wallet.payer.publicKey,
+      splTokenAClient);
+    if(hlaOperatingAtaCreateInstruction !== null)
+      instructions.push(hlaOperatingAtaCreateInstruction);
+
+    if(instructions.length == 0)
+      instructions = undefined;
+    
+    let ownerLamports = await getLamports(ownerAccountAddress);
+    let payerLamports = await getLamports(payer.publicKey);
 
     console.log("TEST PARAMETERS:")
     console.log("  Program ID:                         " + program.programId);
+    console.log("  payer.address:                      " + payer.publicKey);
+    console.log("  payer.lamports:                     %s (%s SOL)", payerLamports, payerLamports / LAMPORTS_PER_SOL);
     console.log("  mintAuthority.publicKey:            " + mintAuthority.publicKey);
     console.log("  mintA.publicKey:                    " + mintA.publicKey);
     console.log("  mintB.publicKey:                    " + mintB.publicKey);
-    console.log("  ownerAccountAddress:                " + provider.wallet.publicKey);
-    console.log("  ownerAccount.lamports:              " + ownerAccountLamports);
-    console.log("  ownerFromTokenAccountAddress:       " + ownerTokenAccountAAddress);
     console.log("  blockHeight:                        " + blockHeight);
+    console.log();
+    console.log("  ownerAccountAddress:                " + ownerAccountAddress);
+    console.log("  ownerAccount.lamports:              %s (%s SOL)", ownerLamports, ownerLamports / LAMPORTS_PER_SOL);
+    console.log("  ownerFromTokenAccountAddress:       " + ownerTokenAccountAAddress);
+    console.log();
     console.log("  ddcaAccountPda:                     " + ddcaAccountPda);
     console.log("  ddcaAccountPdaBump:                 " + ddcaAccountPdaBump);
     console.log("  ddcaFromTokenAccountAddress:        " + ddcaFromTokenAccountAddress);
+    console.log("  ddcaToTokenAccountAddress:          " + ddcaToTokenAccountAddress);
+    console.log();
+    console.log("  DDCA_OPERATING_ACCOUNT_ADDRESS:     " + DDCA_OPERATING_ACCOUNT_ADDRESS);
+    console.log("  ddcaOperatingFromTokenAccountAddress: " + ddcaOperatingFromTokenAccountAddress);
+    console.log();
+    console.log("  HLA_PROGRAM_ADDRESS:               " + HLA_PROGRAM_ADDRESS);
+    console.log("  hlaProtocolAddress:                " + hlaProtocolAddress);
+    console.log("  hlaPoolAddress:                    " + hlaPoolAddress);
+    console.log("  hlaAmmAddress:                     " + hlaAmmAddress);
+    console.log("  HLA_OPERATING_ACCOUNT_ADDRESS:     " + HLA_OPERATING_ACCOUNT_ADDRESS);
+    console.log("  hlaOperatingFromTokenAccountAddress: " + hlaOperatingFromTokenAccountAddress);
+    console.log();
     console.log("  SYSTEM_PROGRAM_ID:                  " + SYSTEM_PROGRAM_ID);
     console.log("  TOKEN_PROGRAM_ID:                   " + TOKEN_PROGRAM_ID);
+    console.log("  ASSOCIATED_TOKEN_PROGRAM_ID:        " + ASSOCIATED_TOKEN_PROGRAM_ID);
     console.log();
-    
-    const createTx = await program.rpc.createDdca(new anchor.BN(blockHeight), ddcaAccountPdaBump,
+
+    const createTx = await program.rpc.create(new anchor.BN(blockHeight), ddcaAccountPdaBump,
       new anchor.BN(ddcaFromInitialAmount), new anchor.BN(ddcaFromAmountPerSwap), new anchor.BN(ddcaIntervalInSeconds), 
+      new anchor.BN(0), 0,
       {
         accounts: {
-          ownerAccount: provider.wallet.publicKey,
+          // owner
+          ownerAccount: ownerAccountAddress,
           ownerFromTokenAccount: ownerTokenAccountAAddress,
+          // ddca
           ddcaAccount: ddcaAccountPda,
           fromMint: mintA.publicKey,
           fromTokenAccount: ddcaFromTokenAccountAddress,
           toMint: mintB.publicKey,
           toTokenAccount: ddcaToTokenAccountAddress,
+          operatingAccount: DDCA_OPERATING_ACCOUNT_ADDRESS,
+          // hybrid liquidity aggregator accounts
+          hlaProgram: HLA_PROGRAM_ADDRESS,
+          hlaOperatingAccount: HLA_OPERATING_ACCOUNT_ADDRESS,
+          hlaOperatingFromTokenAccount: hlaOperatingFromTokenAccountAddress,
+          // system accounts
+          operatingFromTokenAccount: ddcaOperatingFromTokenAccountAddress,
           rent: anchor.web3.SYSVAR_RENT_PUBKEY,
           systemProgram: SYSTEM_PROGRAM_ID,
           tokenProgram: TOKEN_PROGRAM_ID,
           associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID
         },
+        // signers: [ownerAcc],
+        instructions: instructions,
+        // hybrid liquidity aggregator specific amm pool accounts
+        remainingAccounts: [
+          { pubkey: hlaPoolAddress, isWritable: false, isSigner: false },
+          { pubkey: hlaProtocolAddress, isWritable: false, isSigner: false },
+          { pubkey: hlaAmmAddress, isWritable: false, isSigner: false },
+        ],
       }
     );
 
     await logPostTransactionState("Create", createTx);
 
-    // const accounts = await program.account.vaultAccount.all(provider.wallet.publicKey.toBuffer()); //uncomment to list vault accounts owned by current wallet: provider.wallet.publicKey
+    // const accounts = await program.account.vaultAccount.all(ownerAccAddress.toBuffer()); //uncomment to list vault accounts owned by current wallet: ownerAccAddress
     // const accounts = await program.account.vaultAccount.all(anchor.web3.Keypair.generate().publicKey.toBuffer()); //this will fetch empty list
     // console.log(accounts);
 
     const _ddcaAccount = await program.account.ddcaAccount.fetch(ddcaAccountPda);
-    assert.ok(_ddcaAccount.ownerAccAddr.equals(provider.wallet.publicKey));
+    assert.ok(_ddcaAccount.ownerAccAddr.equals(ownerAccountAddress));
     assert.ok((_ddcaAccount.bump = ddcaAccountPdaBump));
     assert.ok((_ddcaAccount.fromTaccAddress = ddcaFromTokenAccountAddress));
 
@@ -177,10 +270,10 @@ describe("ddca", () => {
 
       console.log("AFTER %s:", transactionName.toUpperCase());
 
-      // const tx = await provider.connection.getTransaction(transactionSignature, 'confirmed');
+      // const tx = await provider.connection.getTransaction(transactionSignature, 'recent');
       // console.log(tx);
       
-      console.log("  ownerAccountLamports.lamports:     " + (await getLamports(program.provider.wallet.publicKey)));
+      console.log("  ownerAccountLamports.lamports:     " + (await getLamports(ownerAccountAddress)));
       console.log("  ownerTokenAccount.amount:          " + (await getTokenAAmount(ownerTokenAccountAAddress)));
       console.log();
       console.log("  ddcaAccount.lamports:              " + (await getLamports(ddcaAccountPda)));
@@ -216,3 +309,47 @@ describe("ddca", () => {
   });
 
 });
+
+async function createAtaCreateInstructionIfNotExists(ataAddress, mintAddress, ownerAccountAddress, payerAddress, tokenClient) {
+  try{
+    await tokenClient.getAccountInfo(ataAddress);
+    console.log("ATA: %s for mint: %s already exists", ataAddress, mintAddress);
+  } catch (err) {
+    // INVALID_ACCOUNT_OWNER can be possible if the associatedAddress has
+    // already been received some lamports (= became system accounts).
+    // Assuming program derived addressing is safe, this is the only case
+    // for the INVALID_ACCOUNT_OWNER in this code-path
+    if (
+      err.message === FAILED_TO_FIND_ACCOUNT ||
+      err.message === INVALID_ACCOUNT_OWNER
+    ) {
+      console.log("ATA: %s for mint: %s was not found (%s). Generating 'create' instruction...", ataAddress, mintAddress, err.message);
+      let [_, ataCreateInstruction] = 
+      await createAtaCreateInstruction(ataAddress, mintAddress, ownerAccountAddress, payerAddress);
+      return ataCreateInstruction;
+    } else {
+      throw err;
+    }
+  }
+}
+
+async function createAtaCreateInstruction(ataAddress, mintAddress, ownerAccountAddress, payerAddress) {
+  if(ataAddress === null){
+    ataAddress = await Token.getAssociatedTokenAddress(
+      associatedProgramId=ASSOCIATED_TOKEN_PROGRAM_ID,
+      programId=TOKEN_PROGRAM_ID,
+      mintAddress=mintAddress,
+      owner=ownerAccountAddress,
+    );
+  }
+  const ataCreateInstruction = new anchor.web3.Transaction();
+  ataCreateInstruction.add(Token.createAssociatedTokenAccountInstruction(
+    ASSOCIATED_TOKEN_PROGRAM_ID,
+    TOKEN_PROGRAM_ID,
+    mintAddress,
+    ataAddress,
+    ownerAccountAddress,
+    payerAddress,
+  ));
+  return [ataAddress, ataCreateInstruction];
+}
